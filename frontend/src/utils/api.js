ff56fb791ad0e35c48c7fd6845457c1210061afd
shaziya516 <shaziya01122004@gmail.com>
2026-05-01 02:50:41 +0530

    remove port

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://role-based-demo.onrender.com:5000/api',
+  baseURL: 'https://role-based-demo.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
